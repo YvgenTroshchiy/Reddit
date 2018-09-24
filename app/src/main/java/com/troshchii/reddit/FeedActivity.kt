6abf1c8a0d2f078bfb105d1e7dbfdde909a7cc2a
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2018-09-24 14:58:29 +0300

    Initial commit

---
+++ app/src/main/java/com/troshchii/reddit/FeedActivity.kt
@@ -0,0 +1,13 @@
+package com.troshchii.reddit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+
+class FeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.feed_activity)
+    }
+}
