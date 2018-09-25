c9ad7f0f0f956f96da81d3f8eb2f762e834021e2
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2018-09-25 16:40:53 +0300

    Remove DaggerAppCompatActivity because of "unresolved reference setContentView"

--- app/src/main/java/com/troshchii/reddit/TopNewsActivity.kt
+++ app/src/main/java/com/troshchii/reddit/TopNewsActivity.kt
@@ -1,23 +1,25 @@
 package com.troshchii.reddit
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.troshchii.reddit.di.scope.ActivityScoped
 import com.troshchii.reddit.extensions.getLogTag
 import com.troshchii.reddit.extensions.logW
 import com.troshchii.reddit.extensions.transformer
 import com.troshchii.reddit.network.RedditService
-import dagger.android.support.DaggerAppCompatActivity
+import dagger.android.AndroidInjection
 import javax.inject.Inject
 
 
 @ActivityScoped
-class TopNewsActivity : DaggerAppCompatActivity() {
+class TopNewsActivity : AppCompatActivity() {
 
     private val tag = getLogTag<TopNewsActivity>()
 
     @Inject lateinit var service: RedditService
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.topnews_activity)
 
