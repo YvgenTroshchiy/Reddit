61903b80b26111a868793c2c0b012973ae7593ad
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2018-09-25 16:23:53 +0300

    Update NetworkModels.kt

--- app/src/main/java/com/troshchii/reddit/network/NetworkModels.kt
+++ app/src/main/java/com/troshchii/reddit/network/NetworkModels.kt
@@ -19,8 +19,25 @@ import kotlinx.android.parcel.Parcelize
 ) : Parcelable
 
 @Parcelize data class News(
+    val preview: Preview,
     val title: String,
     val author: String,
     val created_utc: String,
     val num_comments: String
+) : Parcelable
+
+@Parcelize data class Preview(
+    val images: List<Image>,
+    val enabled: Boolean
+) : Parcelable
+
+@Parcelize data class Image(
+    val source: Source,
+    val resolutions: List<Source>
+) : Parcelable
+
+@Parcelize data class Source(
+    val url: String,
+    val width: Int,
+    val height: Int
 ) : Parcelable
