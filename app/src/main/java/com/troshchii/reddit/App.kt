c946fa64f7130a09ef609170e0b0cf6b8fc6317f
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2018-09-27 16:20:49 +0300

    Add LeakCanary

--- app/src/main/java/com/troshchii/reddit/App.kt
+++ app/src/main/java/com/troshchii/reddit/App.kt
@@ -1,5 +1,6 @@
 package com.troshchii.reddit
 
+import com.squareup.leakcanary.LeakCanary
 import com.troshchii.reddit.di.DaggerAppComponent
 import com.troshchii.reddit.extensions.setStrictMode
 import dagger.android.AndroidInjector
@@ -14,6 +15,13 @@ class App : DaggerApplication() {
     override fun onCreate() {
         if (BuildConfig.DEBUG) setStrictMode()
         super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
     }
 
 }
