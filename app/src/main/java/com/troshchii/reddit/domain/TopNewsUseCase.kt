fd007a52ecb0ca141ea9787980d85b424ff09c2a
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2020-05-05 16:22:02 +0300

    Update UseCase

--- app/src/main/java/com/troshchii/reddit/domain/TopNewsUseCase.kt
+++ app/src/main/java/com/troshchii/reddit/domain/TopNewsUseCase.kt
@@ -9,18 +9,17 @@ import com.troshchii.reddit.ui.topnews.data.RedditPost
 import com.troshchii.reddit.ui.topnews.data.toTopNews
 
 
-class TopNewsUseCase(private val service: RedditService) : UseCase() {
+const val LIMIT = 25
+
+
+class TopNewsUseCase(private val service: RedditService) : UseCase<List<RedditPost>, String>() {
 
     private val tag = getLogTag<TopNewsUseCase>()
 
-    companion object {
-        const val limit = 25
-    }
-
-    override suspend fun execute(): Either<Failure, List<RedditPost>> {
+    override suspend fun execute(params: String?): Either<Failure, List<RedditPost>> {
         logI(tag, "execute")
 
-        val result = service.topNews(limit)
+        val result = service.topNews(LIMIT, params)
 
         return if (result.isSuccessful && result.body() != null) {
             Either.Right(result.body()!!.toTopNews())
--- app/src/main/java/com/troshchii/reddit/domain/UseCase.kt
+++ app/src/main/java/com/troshchii/reddit/domain/UseCase.kt
@@ -1,7 +1,14 @@
 package com.troshchii.reddit.domain
 
+import com.troshchii.reddit.core.exception.Failure
+import com.troshchii.reddit.core.functional.Either
+
 
 //TODO: Update
-abstract class UseCase {
-    abstract suspend fun execute(): Any
+abstract class UseCase<out Type, in Params> where Type : Any {
+
+    abstract suspend fun execute(params: Params? = null): Either<Failure, Type>
+
+    class None
+
 }
