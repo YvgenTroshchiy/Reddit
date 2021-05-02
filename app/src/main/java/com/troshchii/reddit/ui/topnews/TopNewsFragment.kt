ba69a1f3c49b638f81bfee77773b6f54b40fb27f
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-05-02 16:43:40 +0300

    Use flow to get topNews

--- app/src/main/java/com/troshchii/reddit/ui/topnews/TopNewsFragment.kt
+++ app/src/main/java/com/troshchii/reddit/ui/topnews/TopNewsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
@@ -14,14 +15,18 @@ import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.transition.MaterialElevationScale
 import com.troshchii.reddit.R
+import com.troshchii.reddit.core.Async
+import com.troshchii.reddit.core.Fail
+import com.troshchii.reddit.core.Success
 import com.troshchii.reddit.core.extensions.getLogTag
 import com.troshchii.reddit.core.extensions.logI
 import com.troshchii.reddit.core.extensions.logW
 import com.troshchii.reddit.core.extensions.observe
 import com.troshchii.reddit.core.extensions.toast
-import com.troshchii.reddit.core.functional.Either
 import com.troshchii.reddit.databinding.TopnewsFragmentBinding
 import com.troshchii.reddit.ui.topnews.data.RedditPost
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.FlowCollector
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TopNewsFragment : Fragment() {
@@ -52,24 +57,46 @@ class TopNewsFragment : Fragment() {
         view.doOnPreDraw { startPostponedEnterTransition() }
     }
 
+    @OptIn(InternalCoroutinesApi::class)
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         logI(logTag, "onActivityCreated")
 
         setupNewsList()
 
-        viewLifecycleOwner.observe(viewModel.topNews) {
-            when (it) {
-                is Either.Left -> {
-                    logW(logTag, "Error: $it")
-                    context?.toast("${it.left}", Toast.LENGTH_LONG)
+//        viewLifecycleOwner.observe(viewModel.topNews) {
+//            when (it) {
+//                is Either.Left -> {
+//                    logW(logTag, "Error: $it")
+//                    context?.toast("${it.left}", Toast.LENGTH_LONG)
+//                }
+//                is Either.Right -> {
+//                    logI(logTag, "Success")
+//                    topNewsAdapter.news = it.right
+//                }
+//            }
+//        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.topNews.collect(object : FlowCollector<Async<List<RedditPost>>> {
+                override suspend fun emit(it: Async<List<RedditPost>>) {
+                    when (it) {
+                        is Fail -> {
+                            logW(logTag, "Error: $it")
+                            context?.toast("${it.error.message}", Toast.LENGTH_LONG)
+                        }
+                        is Success -> {
+                            logI(logTag, "Success")
+                            topNewsAdapter.news = it.invoke()
+                        }
+                        else -> {
+
+                        }
+                    }
                 }
-                is Either.Right -> {
-                    logI(logTag, "Success")
-                    topNewsAdapter.news = it.right
-                }
-            }
+            })
         }
+
         viewLifecycleOwner.observe(viewModel.isLoading) {
             logI(logTag, "isLoading: $it")
             binding.progressBar.visibility = if (it == true) View.VISIBLE else View.GONE
