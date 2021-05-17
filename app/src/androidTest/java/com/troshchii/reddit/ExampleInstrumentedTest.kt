fd5f97fcfee908eece7b14b4d69a6f89d8004a4f
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-05-17 10:50:13 +0300

    update dependencies

--- app/src/androidTest/java/com/troshchii/reddit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/troshchii/reddit/ExampleInstrumentedTest.kt
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
+
+    @Test fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.troshchii.reddit", appContext.packageName)
