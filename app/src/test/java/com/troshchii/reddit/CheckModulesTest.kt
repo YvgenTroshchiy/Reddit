e4d3b941943e827f56bd1cb2445c21b38cfe8536
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-07-29 15:48:01 +0300

    Update CheckModulesTest

--- app/src/test/java/com/troshchii/reddit/CheckModulesTest.kt
+++ app/src/test/java/com/troshchii/reddit/CheckModulesTest.kt
@@ -1,24 +1,26 @@
 package com.troshchii.reddit
 
-import androidx.test.platform.app.InstrumentationRegistry
 import com.troshchii.reddit.di.allAppModules
 import org.junit.Ignore
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.koin.android.ext.koin.androidContext
-import org.koin.dsl.koinApplication
 import org.koin.test.KoinTest
 import org.koin.test.check.checkModules
+import org.koin.test.mock.MockProviderRule
+import org.mockito.Mockito
 
-@RunWith(AndroidJUnit4ClassRunner::class)
 class CheckModulesTest : KoinTest {
 
-    // TODO: Fix test
+    @get:Rule
+    val mockProvider = MockProviderRule.create { clazz ->
+        Mockito.mock(clazz.java)
+    }
+
+    // TODO: Investigate why it's fails
     @Ignore
-    @Test fun checkAllModules() {
-        koinApplication {
-            modules(allAppModules)
-            androidContext(InstrumentationRegistry.getInstrumentation().context)
-        }.checkModules()
+    @Test
+    fun checkAllModules() = checkModules {
+        modules(allAppModules)
+        // other KoinApplication attributes if needed
     }
 }
