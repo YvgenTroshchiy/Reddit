f7b2250ba527a8f8074e4cfe41f340717ad1d2b2
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-11-19 17:33:00 +0200

    Add AppComponent, AppModule.kt, NetworkModule.kt

---
+++ app/src/main/java/com/troshchii/reddit/di/dagger/AppModule.kt
@@ -0,0 +1,8 @@
+package com.troshchii.reddit.di.dagger
+
+import com.google.android.datatransport.runtime.dagger.Module
+
+@Module(includes = [NetworkModule::class])
+class AppModule {
+
+}
---
+++ app/src/main/java/com/troshchii/reddit/di/dagger/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.troshchii.reddit.di.dagger
+
+import android.content.Context
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.troshchii.reddit.BuildConfig
+import com.troshchii.reddit.network.RedditService
+import dagger.Module
+import dagger.Provides
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.BASIC
+import okhttp3.logging.HttpLoggingInterceptor.Level.NONE
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
+
+private const val TIMEOUT = 30L
+
+@Module class NetworkModule {
+
+    @Provides fun provideRedditService(retrofit: Retrofit): RedditService {
+        return retrofit.create(RedditService::class.java)
+    }
+
+    @Provides fun retrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+    @Provides fun okHttpClient(cache: Cache): OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor().apply { level = if (BuildConfig.DEBUG) BASIC else NONE })
+            .cache(cache)
+            .build()
+
+    @Provides fun cache(cacheFile: File) = Cache(cacheFile, (10 * 1024 * 1024 /* 10MB Cache */).toLong())
+
+    @Provides fun cacheFile(context: Context): File = File(context.cacheDir, "okhttp_cache")
+
+    @Provides fun gson(): Gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+
+}
