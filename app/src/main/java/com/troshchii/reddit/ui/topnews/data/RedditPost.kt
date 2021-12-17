cc8721f9319fe0fdf86b0df6e44d0cdb4a59967e
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-12-17 19:00:24 +0200

    Fix Parcelize inport for RedditPost.kt

--- app/src/main/java/com/troshchii/reddit/ui/topnews/data/RedditPost.kt
+++ app/src/main/java/com/troshchii/reddit/ui/topnews/data/RedditPost.kt
@@ -1,7 +1,7 @@
 package com.troshchii.reddit.ui.topnews.data
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize data class RedditPost(
     val id: String,
